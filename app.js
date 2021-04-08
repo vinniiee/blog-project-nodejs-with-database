add39a5503b0428f7f8c2c7601cca06e0de90541
vinniiee <vs28245@gmail.com>
2021-04-08 16:39:58 +0530

    Added MongoDB

--- app.js
+++ app.js
@@ -2,83 +2,212 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const _ = require('lodash');
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const mongoose = require("mongoose");
+const homeStartingContent = "Start contributing to the blog by clickng on COMPOSE. Later on you will be able to update and delete blog posts by accessing posts through this same home page.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true
+});
 
-posts = [];
+const postsSchema = ({
+  "title": {
+    type: String,
+    required: true,
+  },
+  "content": {
+    type: String,
+  }
+});
 
-app.set('view engine','ejs');
+const post = mongoose.model("Post", postsSchema);
 
-app.use(bodyParser.urlencoded({extended:true}));
+
+
+// post.insertOne(homeContent,function(err){
+//
+//   if(err){
+//     console.log(err);
+//   }
+//   else{
+//     console.log("Successfully added homeContent");
+//   }
+//
+// });
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("static"));
 
 
 
-app.get("/",function(req,res){
+app.get("/", function(req, res) {
 
-  res.render("home.ejs",{start:homeStartingContent,postList:posts});
+  post.find({}, function(err, result) {
+    console.log(result.length);
+    // console.log(result.length);
+    if (result.length == 0) {
+      const home = new post({
+        title: "How to Start?",
+        content: homeStartingContent,
+      });
+      home.save();
+      return res.render("home.ejs", {
+        postList: [home]
+      });
+      // res.render("home.ejs",);
+      // res.render("home.ejs",{postList:[homeStartingContent]})
+    } else {
+          console.log(result);
+      return res.render("home.ejs", {
+
+        postList: result
+      });
+    } // else if (result.length>1 & result[0].title=="How to Start?"){
+    //
+    // post.findByIdAndRemove()
+    //
+    // }
+
+  });
+  // res.render("home.ejs",{start:homeStartingContent,postList:posts});
 
 });
 
-app.get("/about",function(req,res){
-
-  res.render("about.ejs",{aboutContent:aboutContent});
+app.post("/delete",function(req,res){
+    title = req.body.title
+    post.deleteOne({title:title},function(err){
+      if(err){
+        console.log(err)
+      }
+      else{
+        console.log("Deleted "+title+" successfully!" );
+      }
+    });
+    res.redirect("/")
 
 });
 
-app.get("/contact",function(req,res){
+app.get("/about", function(req, res) {
 
-  res.render("contact.ejs",{contactContent:contactContent});
+  res.render("about.ejs", {
+    aboutContent: aboutContent
+  });
 
 });
 
-app.get("/compose",function(req,res){
+app.get("/contact", function(req, res) {
+
+  res.render("contact.ejs", {
+    contactContent: contactContent
+  });
+
+});
+
+app.get("/compose", function(req, res) {
 
   res.render("compose.ejs");
 
 });
 
-app.get("/posts/:postName",function(req,res){
+app.get("/posts/:postName", function(req, res) {
 
   const requestedTitle = _.lowerCase(req.params.postName);
   console.log(requestedTitle);
-  posts.forEach(function(post){
-    var storedTitle = _.lowerCase(post.title);
-    console.log("2"+storedTitle);
+  post.find({}, function(err, result) {
 
-    if ( storedTitle === requestedTitle ){
-      console.log(storedTitle+"="+requestedTitle);
-      return res.render("post.ejs", {
-    title: post.title,
-    content: post.content,
-    })
-      }
+      result.forEach(function(post) {
+          var storedTitle = _.lowerCase(post.title);
+          console.log("2" + storedTitle);
+          if (storedTitle === requestedTitle) {
+            console.log(storedTitle + "=" + requestedTitle);
+            return res.render("post.ejs", {
+              title: post.title,
+              content: post.content,
+            });
+
+
+          }
+      });
 
   });
 
 });
 
-app.post("/compose",function(req,res){
+app.post("/compose", function(req, res) {
+
+  const title = req.body.title
+  const content = req.body.content
+  console.log("this is content"+content)
+  // const filter = {title:title};
+  // const update = {content: content};
+  //
+  //  post.findOneAndUpdate(filter,update,{
+  //   new:true,
+  //   upsert:true,
+  // });
+  post.find({title:title},function(err,result){
+
+    if(result.length==0){
+      const newPost = new post({
+        content: content,
+        title: title,
+      });
+
+      newPost.save();
+    }
+    else{
+        post.updateOne({title:title}, {content: content}, function(err){
+          if(err){
+            console.log(err);
+          }
+          else{
+            console.log("updated")
+          }
+        });
+        // result.content = content;
+        // result.save();
+    }
+
+  });
+
+  post.findOne({title: "How to Start?"}, function(err, result) {
+    // console.log(result.id)
+    if (result) {
+      post.findByIdAndRemove(result.id, function(err) {});
+    }
 
 
-  const post = {
-    content: req.body.postContent,
-    title: req.body.title,
-  };
 
-  posts.push(post);
+  });
+
   res.redirect("/")
 
 });
 
+app.post("/update",function(req,res){
 
+    const title = req.body.title;
+    post.findOne({title:title},function(err,result){
+        console.log(result.content);
+      res.render("update.ejs",{
+        title:result.title,
+        content:result.content,
+      });
+    });
+    // res.render("update.ejs",{
+    //   title:req.body.title;
+    //   content:req.body.content
+    // })
 
-app.listen(3000,function(){
+});
+
+app.listen(3000, function() {
 
   console.log("Listening at port 3000...")
 
